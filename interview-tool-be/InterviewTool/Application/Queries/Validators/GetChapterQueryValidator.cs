2348b44b68799e456da5ec05551b08920878fd09
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-08-30 22:57:34 +0300

    Added chapter query handler

---
+++ interview-tool-be/InterviewTool/Application/Queries/Validators/GetChapterQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace InterviewTool.Application.Queries.Validators
+{
+    public class GetChapterQueryValidator : AbstractValidator<GetChapterQuery>
+    {
+        public GetChapterQueryValidator()
+        {
+            RuleFor(x => x.ChapterId).GreaterThan(0);
+        }
+    }
+}
---
+++ interview-tool-be/InterviewTool/Application/QueryHandlers/ChapterQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using InterviewTool.Application.DTOs;
+using InterviewTool.Application.Models;
+using InterviewTool.Application.Queries;
+using InterviewTool.Domain.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InterviewTool.Application.QueryHandlers
+{
+    public class ChapterQueryHandler
+        : IRequestHandler<GetChapterQuery, ExecutionResult<ChapterDTO>>
+    {
+        private readonly IUnitOfWork _uow;
+
+        public ChapterQueryHandler(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public Task<ExecutionResult<ChapterDTO>> Handle(GetChapterQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
