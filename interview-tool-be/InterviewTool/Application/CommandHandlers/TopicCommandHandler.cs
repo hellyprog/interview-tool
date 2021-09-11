6d2db2d7d2517810eb074c36525ba12251cb0ae2
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-09-11 12:55:09 +0300

    Added topic command handler implementation

--- interview-tool-be/InterviewTool/Application/CommandHandlers/TopicCommandHandler.cs
+++ interview-tool-be/InterviewTool/Application/CommandHandlers/TopicCommandHandler.cs
@@ -1,6 +1,50 @@
-﻿namespace InterviewTool.Application.CommandHandlers
+﻿using InterviewTool.Application.Commands;
+using InterviewTool.Application.Models;
+using InterviewTool.Domain.Entities;
+using InterviewTool.Domain.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InterviewTool.Application.CommandHandlers
 {
-    public class TopicCommandHandler
+    public class TopicCommandHandler :
+        IRequestHandler<CreateTopicCommand, ExecutionResult>,
+        IRequestHandler<UpdateTopicCommand, ExecutionResult>,
+        IRequestHandler<DeleteTopicCommand, ExecutionResult>
     {
+        private readonly IUnitOfWork _uow;
+
+        public TopicCommandHandler(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public async Task<ExecutionResult> Handle(CreateTopicCommand request, CancellationToken cancellationToken)
+        {
+            var topicToInsert = new Topic(request.Name, request.Weight);
+            _uow.TopicRepository.Insert(topicToInsert);
+            var result = await _uow.SaveAsync();
+
+            return result > 0 ? ExecutionResult.FromSuccess() : ExecutionResult.FromFailure("Error saving topic");
+        }
+
+        public async Task<ExecutionResult> Handle(UpdateTopicCommand request, CancellationToken cancellationToken)
+        {
+            var topicToUpdate = await _uow.TopicRepository.GetByIdAsync(request.TopicId);
+            topicToUpdate.UpdateName(request.Name);
+            topicToUpdate.UpdateWeight(request.Weight);
+            var result = await _uow.SaveAsync();
+
+            return result > 0 ? ExecutionResult.FromSuccess() : ExecutionResult.FromFailure("Error updating topic");
+        }
+
+        public async Task<ExecutionResult> Handle(DeleteTopicCommand request, CancellationToken cancellationToken)
+        {
+            await _uow.TopicRepository.DeleteByIdAsync(request.TopicId);
+            var result = await _uow.SaveAsync();
+
+            return result > 0 ? ExecutionResult.FromSuccess() : ExecutionResult.FromFailure("Error deleting topic");
+        }
     }
 }
