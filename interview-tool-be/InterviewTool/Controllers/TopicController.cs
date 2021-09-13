c7ab60910aa15e6bf8eb7a2a71563fadc8ba9658
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-09-13 14:36:22 +0300

    Added topic controller

--- interview-tool-be/InterviewTool/Controllers/TopicController.cs
+++ interview-tool-be/InterviewTool/Controllers/TopicController.cs
@@ -1,6 +1,64 @@
-﻿namespace InterviewTool.Controllers
+﻿using InterviewTool.Application.Commands;
+using InterviewTool.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace InterviewTool.Controllers
 {
-    public class TopicController
+    public class TopicController : BaseApplicationController
     {
+        private readonly IMediator _mediator;
+
+        public TopicController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTopics()
+        {
+            var result = await _mediator.Send(new GetTopicsQuery());
+            var statusCode = result.Success ? StatusCodes.Status200OK : StatusCodes.Status400BadRequest;
+
+            return StatusCode(statusCode, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTopic(int topicId)
+        {
+            var result = await _mediator.Send(new GetTopicQuery { TopicId = topicId });
+            var statusCode = result.Success ? StatusCodes.Status200OK : StatusCodes.Status400BadRequest;
+
+            return StatusCode(statusCode, result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTopic([FromBody] CreateTopicCommand command)
+        {
+            var result = await _mediator.Send(command);
+            var statusCode = result.Success ? StatusCodes.Status201Created : StatusCodes.Status400BadRequest;
+
+            return StatusCode(statusCode, result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateTopic([FromBody] UpdateTopicCommand command)
+        {
+            var result = await _mediator.Send(command);
+            var statusCode = result.Success ? StatusCodes.Status200OK : StatusCodes.Status400BadRequest;
+
+            return StatusCode(statusCode, result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTopic(int topicId)
+        {
+            var result = await _mediator.Send(new DeleteTopicCommand { TopicId = topicId });
+            var statusCode = result.Success ? StatusCodes.Status200OK : StatusCodes.Status400BadRequest;
+
+            return StatusCode(statusCode, result);
+        }
     }
 }
