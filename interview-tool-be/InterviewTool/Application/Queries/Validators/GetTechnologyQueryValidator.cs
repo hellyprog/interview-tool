f35466869e02a34227d2fd9146345c74a4c53279
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-10-18 22:56:27 +0300

    Added technology query handler

---
+++ interview-tool-be/InterviewTool/Application/Queries/Validators/GetTechnologyQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using InterviewTool.Application.Queries.Technology;
+
+namespace InterviewTool.Application.Queries.Validators
+{
+    public class GetTechnologyQueryValidator : AbstractValidator<GetTechnologyQuery>
+    {
+        public GetTechnologyQueryValidator()
+        {
+            RuleFor(x => x.TechnologyId).GreaterThan(0);
+        }
+    }
+}
--- interview-tool-be/InterviewTool/Application/QueryHandlers/TechnologyQueryHandler.cs
+++ interview-tool-be/InterviewTool/Application/QueryHandlers/TechnologyQueryHandler.cs
@@ -1,6 +1,44 @@
-﻿namespace InterviewTool.Application.QueryHandlers
+﻿using AutoMapper;
+using InterviewTool.Application.DTOs;
+using InterviewTool.Application.Models;
+using InterviewTool.Application.Queries.Technology;
+using InterviewTool.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InterviewTool.Application.QueryHandlers
 {
-    public class TechnologyQueryHandler
+    public class TechnologyQueryHandler :
+        IRequestHandler<GetTechnologyQuery, ExecutionResult<TechnologyDTO>>,
+        IRequestHandler<GetTechnologiesQuery, ExecutionResult<List<TechnologyDTO>>>
     {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public TechnologyQueryHandler(IUnitOfWork uow, IMapper mapper)
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        public async Task<ExecutionResult<TechnologyDTO>> Handle(GetTechnologyQuery request, CancellationToken cancellationToken)
+        {
+            var technology = await _uow.TechnologyRepository.GetByIdAsync(request.TechnologyId);
+
+            return technology != null
+                ? ExecutionResult<TechnologyDTO>.FromSuccess(_mapper.Map<TechnologyDTO>(technology))
+                : ExecutionResult<TechnologyDTO>.FromFailure("Error getting technology");
+        }
+
+        public async Task<ExecutionResult<List<TechnologyDTO>>> Handle(GetTechnologiesQuery request, CancellationToken cancellationToken)
+        {
+            var technologies = await _uow.TechnologyRepository.GetAll()
+                .ToListAsync(cancellationToken: cancellationToken);
+
+            return ExecutionResult<List<TechnologyDTO>>.FromSuccess(_mapper.Map<List<TechnologyDTO>>(technologies));
+        }
     }
 }
